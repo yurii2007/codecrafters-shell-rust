1fec1285a195767ba8e0e8c7d7f8a7dd066206f7
yurii2007 <yurash862@gmail.com>
2025-05-24 19:03:18 +0300

    echo builtin

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,21 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        match input.trim() {
-            "exit 0" => process::exit(0),
+        let (command, user_input) = extract_command(&mut input);
+
+        match command {
+            "echo" => println!("{}", user_input.unwrap_or("")),
+            "exit" => process::exit(0),
             _ => println!("{}: command not found", input.trim()),
         }
+    }
+}
+
+fn extract_command(input: &mut String) -> (&str, Option<&str>) {
+    let parsed_input = input.trim().split_once(" ");
+
+    match parsed_input {
+        Some((command, user_input)) => (command, Some(user_input)),
+        None => (input.trim(), None),
     }
 }
